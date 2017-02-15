466aece6d0f878c849773cc93f35607b1eb751ef
Fabian Ihle <fabian_ihle@t-online.de>
2017-02-15 11:24:19 +0100

    Added PvP and WvW dailies

--- gw2dailies.sh
+++ gw2dailies.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Check requirements
+
+if ! type "jq" > /dev/null; then
+	echo "jq not found!"
+	exit 1
+fi
 
 DIR=/opt/gw2api
 
@@ -9,9 +15,10 @@ TMPID=$DIR/ids.tmp
 TMPINFO=$DIR/info.tmp
 
 curl -s https://api.guildwars2.com/v2/achievements/daily | jq .pve > $TMPPVE
+curl -s https://api.guildwars2.com/v2/achievements/daily | jq .pvp > $TMPPVP
+curl -s https://api.guildwars2.com/v2/achievements/daily | jq .wvw > $TMPWVW
 
 getID(){
-#ids=$(cat $1 | grep id | egrep -o "[0-9]{1,4}" | sed 's/ /\n/g')
 cat $1 | grep id | egrep -o "[0-9]{1,4}" | sed 's/ /\n/g' > $TMPID
 }
 
@@ -23,18 +30,23 @@ cat $TMPID | while read id; do
 	req=$(grep "requirement" $TMPINFO | sed 's/"requirement": "//g' | sed 's/",//g')
 	
 	# Ausgabe
-	echo "-----------------------"
+	#echo "-----------------------"
 	echo "$name"
 	echo "$descr"
 	echo "$req"
 	echo -e "\n"
 	echo "-----------------------"
 done
-
-echo $name
 }
 
+echo "PvE:"
 getID $TMPPVE
+getInfo
+echo "PvP:"
+getID $TMPPVP
+getInfo
+echo "WvW:"
+getID $TMPWVW
 getInfo
 
 rm -f $DIR/*.tmp
